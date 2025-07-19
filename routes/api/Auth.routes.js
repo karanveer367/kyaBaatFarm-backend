6b9c67898b58b8a73b20fbc95df4d513e65a4319
Karanveer Singh <maankaran049@gmail.com>
2025-07-19 10:41:40 +0530

    chnge

--- routes/api/Auth.routes.js
+++ routes/api/Auth.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const login = require("../../controllers/Auth/login");
 const register = require("../../controllers/Auth/register");
+const addproduct=require("../../controllers/product/addproduct");
 
 router.post("/login", login);
 router.post("/register",register);
